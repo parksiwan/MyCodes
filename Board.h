0646de4b71e3100203b26b45fc155422eb8011b1
parksiwan <parksiwan@users.noreply.github.com>
2017-01-02 22:50:56 +1100

    Create Board.h

---
+++ Board.h
@@ -0,0 +1,25 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include "Window.h"
+#include <string>
+
+class Board : public Window 
+{
+public:
+	Board(const Window& window, int w, int h, int x, int y, int r, int g, int b, int a);
+	Board(const Window& window, int w, int h, int x, int y, int option);
+	~Board();
+
+	void draw();
+
+private:
+	int w_, h_;
+	int x_, y_;
+	int r_, g_, b_, a_;
+	short option_ = 0;
+	SDL_Surface* label_ = nullptr;
+	SDL_Texture* texture_ = nullptr;
+};
+
+#endif
