fa9580c8772eee58e746e7c33f3c4689ba6e8529
parksiwan <parksiwan@users.noreply.github.com>
2017-01-05 23:07:29 +1100

    Update Game-draft.h

--- Game-draft.h
+++ Game-draft.h
@@ -1,17 +1,25 @@
 #ifndef GAME_H
 #define GAME_H
 
-const short FIELD_H = 5;
-const short FIELD_V = 5;
+#include "Tetrads.h"
+
+const short FIELD_H = 20;
+const short FIELD_V = 10;
+const short TETRAD_H = 5;
+const short TETRAD_V = 5;
 
 class Game
 {
 public:
-	Game(short** tetrad, short x, short y, short type);
+	Game(short level);
 	~Game();
 	void checkBoundary();
+	void updateGame(Tetrads* tts, short x, short y, short startRow);
 
 private:
+	int lines_;
+	int scores_;
+	short level_;
 	short field_[FIELD_H][FIELD_V] = { { 0 } };
 };
 
