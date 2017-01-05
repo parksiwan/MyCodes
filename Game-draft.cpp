db33c9f6215853d26eb541b7339d3ec2027a0303
parksiwan <parksiwan@users.noreply.github.com>
2017-01-05 23:06:52 +1100

    Update Game-draft.cpp

--- Game-draft.cpp
+++ Game-draft.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "Game.h"
 
-Game::Game(short** tetrad, short x, short y)
+Game::Game(short level)
 {
-	for (int i = x; i < FIELD_H; i++)
+	level_ = level;
+	lines_ = 0;
+	scores_ - 0;
+	for (int i = 0; i < FIELD_H; i++)
 	{
-		for (int j = y; j < FIELD_V; j++)
-			field_[i][j] = tetrad[i][j];
+		for (int j = 0; j < FIELD_V; j++)
+			field_[i][j] = 0;
 	}
 }
 
@@ -18,4 +21,13 @@ Game::~Game()
 void Game::checkBoundary()
 {
 
+}
+
+void Game::updateGame(Tetrads* tts, short x, short y, short startRow)
+{
+	for (int i = startRow; i < TETRAD_H; i++)
+	{
+		for (int j = 0; j < TETRAD_V; j++)
+			field_[x + i][y + j] = tts->tetrad_[i][j];
+	}
 }
