480a31e394d36ebabf5a1baecb8ea021ab894cea
parksiwan <parksiwan@users.noreply.github.com>
2017-01-10 22:45:26 +1100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,36 +31,44 @@ void startBoard(Window window)
 	Board label_exit(window, 50, 50, 420, 300, 10);
 }
 
+void getTetrad(int type, int rotation, short tetrad[][VERTICAL])
+{
+	for (int i = 0; i < HORIZONTAL; i++)
+	{
+		for (int j = 0; j < VERTICAL; j++)
+			tetrad[i][j] = tetrads[type][rotation][i][j];
+	}
+}
+
 int main(int argc, char* args[]) 
 {
-	short curType, nextType, type;
-	Window window("AIT Tetris", 800, 600);
+	int curType, nextType, type;
+	short nTetrad[HORIZONTAL][VERTICAL] = { { 0 } };
+	Window window("AIT Tetris", W_WIDTH, W_HEIGHT);
+	Game* tGame = new Game(1);
+	Board gameBoard(window, 20, 20, 300, 200, colorImage[0], tGame);    //Display board and tetrads
 	
-	Board gameBoard(window, 200, 400, 300, 200, 255, 255, 255, 1);      //Display game main board
+	Board mainBoard(window, 200, 400, 300, 200, 255, 255, 255, 1);      //Display game main board
 	Board labelBoard(window, 50, 50, 300, 100, 1);                      //Label above the main board
 
-	Board rightSubBoard(window, 50, 50, 300, 100, 1);
-	Board labelLevel(window, 150, 50, 600, 200, 2);
-	Board labelScore(window, 150, 50, 600, 250, 3);
-	Board labelLines(window, 150, 50, 600, 300, 4);
+	Board labelLevel(window, 150, 50, 600, 200, 2);                     //"Level" label inside the sub-board
+	Board labelScore(window, 150, 50, 600, 250, 3);                     //"Score" label inside the sub-board
+	Board labelLines(window, 150, 50, 600, 300, 4);                     //"Lines" label inside the sub-board
 
-	Board leftSubBoard(window, 100, 100, 100, 200, 255, 255, 255, 1);  //Sub board on the left
-	Board labelNext(window, 150, 50, 120, 170, 5);
+	Board leftSubBoard(window, 100, 100, 100, 200, 255, 255, 255, 1);   //Sub board on the left
+	Board labelNext(window, 150, 50, 120, 170, 5);                      //"Next" label above the sub-board
 
-	Game Tetris(1);
 	srand(time(NULL));
 	curType = rand() % TYPE;
 	while (!window.isClosed())
 	{
 		nextType = rand() % TYPE;
-		type = curType;              //Always curType will be 'type'
-		curType = nextType;          //nextType will be curType at next loop
-		Tetrads* ptrTetrad = new Tetrads(type, 0);
+		getTetrad(nextType, 0, nTetrad);
+		Board next_Tetrads(window, 20, 20, 100, 200, colorImage[nextType]);
 
-		//Tetrads next_Tetrads(20, 20, 130, 230, "green_piece.png", 5, 0);
-		//Tetrads Tetrads(window, 40, 40, 300, 200, 1,1,1,1);
-		//Tetrads Tetrads(20, 20, 300, 200, "darkgreen_piece.png", 1, 0);
-
+		type = curType;                                 //'type' is 'curType'
+		curType = nextType;                             //'nextType' will be 'curType' at next loop
+		Tetrads* ptrTetrad = new Tetrads(type, 0);      //A pointer to Tetrads is generated with the 'type'
 		while (!ptrTetrad->isEnded())
 		{
 			if (pollEvents(window, ptrTetrad) == true)
@@ -72,18 +80,26 @@ int main(int argc, char* args[])
 				ptrTetrad->movedownTedrads();
 			}
 
-			next_Tetrads.draw();
+			ptrTetrad->movedownTedrads();
+			tGame->updateGame(ptrTetrad);
+						
 			labelBoard.draw();
-			gameBoard.draw();
-
 			labelLevel.draw();
 			labelScore.draw();
 			labelLines.draw();
 			labelNext.draw();
 			leftSubBoard.draw();
+			next_Tetrads.draw(nTetrad);
+			mainBoard.draw();
+
+			gameBoard.draw(tGame);
+
 			window.clear();
-			Sleep(400);
+			Sleep(200);
 		}
+		if (ptrTetrad != nullptr)
+			delete ptrTetrad;
 	}
+	delete tGame;
 	return 0;
 }
