0582945255d2c34d7cf3ed363e92b10fc0cf05a0
parksiwan <parksiwan@users.noreply.github.com>
2017-01-10 22:39:33 +1100

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -71,7 +71,7 @@ Board::Board(const Window& window, int w, int h, int x, int y, int option) : Win
 	}
 
 	if (font == nullptr) 
-		std::cerr << "Font init fail" << std::endl;
+		std::cerr << "Font init fail---" << std::endl;
 
 	if (!label_)
 		std::cerr << "Failed to create surface" << std::endl;
@@ -83,10 +83,44 @@ Board::Board(const Window& window, int w, int h, int x, int y, int option) : Win
 	
 	SDL_QueryTexture(texture_, NULL, NULL, &w_, &h_);
 }
-	
-Board::Board(const Window& window, int w, int h, int x, int y, short** field) : Window(window)
-{
 
+Board::Board(const Window& window, int w, int h, int x, int y, const std::string& imagePath) : Window(window)
+{
+	w_ = w;
+	h_ = h;
+	x_ = x;
+	y_ = y;
+	auto surface = IMG_Load(imagePath.c_str());
+
+	if (!surface)
+		std::cerr << "Failed to create surface" << std::endl;
+
+	texture_ = SDL_CreateTextureFromSurface(renderer_, surface);
+
+	if (!texture_)
+		std::cerr << "Failed to create texture" << std::endl;
+
+	SDL_FreeSurface(surface);
+}
+
+//Constructor : generates
+Board::Board(const Window& window, int w, int h, int x, int y, const std::string& imagePath, Game* g) : Window(window)
+{
+	w_ = w;
+	h_ = h;
+	x_ = x;
+	y_ = y;
+	auto surface = IMG_Load(imagePath.c_str());
+
+	if (!surface)
+		std::cerr << "Failed to create surface" << std::endl;
+
+	texture_ = SDL_CreateTextureFromSurface(renderer_, surface);
+
+	if (!texture_)
+		std::cerr << "Failed to create texture" << std::endl;
+
+	SDL_FreeSurface(surface);
 }
 
 Board::~Board() 
@@ -107,4 +141,57 @@ void Board::draw()
 		SDL_RenderFillRect(renderer_, &board_rect);
 	}
 }
+
+void Board::draw(short tt[][SB_WIDTH])
+{
+	if (texture_)
+	{
+		for (int i = 0; i < SB_HEIGHT; i++)
+		{
+			for (int j = 0; j < SB_WIDTH; j++)
+			{
+				if (tt[i][j] != 0)
+				{
+					//each "ttRect" consists of a Tetrad
+					//std::cout << "x_ + = " << x_ << " " << "y_ = "  << y_ << std::endl;
+					SDL_Rect ttRect = { x_ + j * 20, y_ + i * 20, w_, h_ };
+					SDL_RenderCopy(renderer_, texture_, nullptr, &ttRect);
+					//std::cout << "i = " << i << "  " << "j= " << j << "  " << "tt[i][j] = " << tt[i][j] << std::endl;
+				}
+			}
+		}
+	}
+	else
+	{
+		SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
+		//SDL_RenderFillRect(renderer_, &ttRect);
+	}
+}
+
+void Board::draw(Game* g)
+{
+	if (texture_)
+	{
+		for (int i = 0; i < F_HEIGHT; i++)
+		{
+			for (int j = 0; j < F_WIDTH; j++)
+			{
+				if (g->field_[i][j] != 0)
+				{
+					//each "ttRect" consists of a Tetrad
+					//std::cout << "x_ + = " << x_ << " " << "y_ = "  << y_ << std::endl;
+					SDL_Rect ttRect = { x_ + j * 20, y_ + i * 20, w_, h_ };
+					SDL_RenderCopy(renderer_, texture_, nullptr, &ttRect);
+					//std::cout << "i = " << i << "  " << "j= " << j << "  " << "tt[i][j] = " << tt[i][j] << std::endl;
+				}
+			}
+		}
+	}
+	else
+	{
+		SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
+		//SDL_RenderFillRect(renderer_, &ttRect);
+	}
+}
+
 
