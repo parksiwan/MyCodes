445c91169bddfe222316e72b90efbe2ae7e964d7
parksiwan <parksiwan@users.noreply.github.com>
2017-01-30 21:24:39 +1100

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -2,9 +2,10 @@
 #include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <iostream>
+#include <string>
 
-Board::Board(const Window& window, int w, int h, int x, int y, int r, int g, int b, int a) : Window(window)
-{
+//Constructor for normal rectangle shape entity
+Board::Board(const Window& window, int w, int h, int x, int y, int r, int g, int b, int a) : Window(window) {
 	w_ = w;
 	h_ = h;
 	x_ = x;
@@ -15,77 +16,43 @@ Board::Board(const Window& window, int w, int h, int x, int y, int r, int g, int
 	a_ = a;
 }
 
-Board::Board(const Window& window, int w, int h, int x, int y, int option) : Window(window)
-{
+//Constructor for label entity
+Board::Board(const Window& window, int w, int h, int x, int y, char* label) : Window(window) {
 	w_ = w;
 	h_ = h;
 	x_ = x;
 	y_ = y;
-	option_ = option;
-
+	
 	TTF_Font* font = nullptr;   //opens font style and sets size
 	SDL_Color color = { 0, 0, 0 };
 
-	switch (option_) 
-	{
-		case 1:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 35);
-			label_ = TTF_RenderText_Solid(font, "AIT TETRIS", color);
-			break;
-		case 2:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "LEVEL", color);
-			break;
-		case 3:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "SCORE", color);
-			break;
-		case 4:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "LINES", color);
-			break;
-		case 5:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "NEXT", color);
-			break;
-		case 6:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "PLAY GAME ?", color);
-			break;
-		case 7:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "GAME ENDS", color);
-			break;
-		case 8:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "START", color);
-			break;
-		case 9:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "RESTART", color);
-			break;
-		case 10:
-			font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
-			label_ = TTF_RenderText_Solid(font, "EXIT", color);
-			break;
-	}
+	font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
+	label_ = TTF_RenderText_Solid(font, label, color);
 
-	if (font == nullptr) 
+	if (font == nullptr)
 		std::cerr << "Font init fail---" << std::endl;
 
 	if (!label_)
 		std::cerr << "Failed to create surface" << std::endl;
-	
+
 	texture_ = SDL_CreateTextureFromSurface(renderer_, label_);
-	
+
 	if (!texture_)
 		std::cerr << "Failed to create texture" << std::endl;
-	
+
 	SDL_QueryTexture(texture_, NULL, NULL, &w_, &h_);
 }
 
-Board::Board(const Window& window, int w, int h, int x, int y, const std::string& imagePath) : Window(window)
-{
+//Constructor for numbers to be displayed inside window
+Board::Board(const Window& window, int w, int h, int x, int y) : Window(window) {
+	w_ = w;
+	h_ = h;
+	x_ = x;
+	y_ = y;
+}
+
+//Constructor for image comprising a tetrad
+Board::Board(const Window& window, int w, int h, int x, int y, const std::string& imagePath) : Window(window) {
 	w_ = w;
 	h_ = h;
 	x_ = x;
@@ -103,9 +70,8 @@ Board::Board(const Window& window, int w, int h, int x, int y, const std::string
 	SDL_FreeSurface(surface);
 }
 
-//Constructor : generates
-Board::Board(const Window& window, int w, int h, int x, int y, const std::string& imagePath, Game* g) : Window(window)
-{
+//Constructor for game field
+Board::Board(const Window& window, int w, int h, int x, int y, const std::string& imagePath, Game* g) : Window(window) {
 	w_ = w;
 	h_ = h;
 	x_ = x;
@@ -123,69 +89,84 @@ Board::Board(const Window& window, int w, int h, int x, int y, const std::string
 	SDL_FreeSurface(surface);
 }
 
-Board::~Board() 
-{
+Board::~Board() {
 	SDL_DestroyTexture(texture_);
 	SDL_FreeSurface(label_);
 }
 
-void Board::draw() 
-{
+void Board::draw() {
 	SDL_Rect board_rect = { x_, y_, w_, h_ };
 
 	if (texture_)
 		SDL_RenderCopy(renderer_, texture_, nullptr, &board_rect);
-	else 
-	{
+	else {
 		SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
 		SDL_RenderFillRect(renderer_, &board_rect);
 	}
 }
 
-void Board::draw(short tt[][SB_WIDTH])
-{
-	if (texture_)
-	{
-		for (int i = 0; i < SB_HEIGHT; i++)
-		{
-			for (int j = 0; j < SB_WIDTH; j++)
-			{
-				if (tt[i][j] != 0)
-				{
+void Board::draw(short tt[][SB_WIDTH]) {
+	if (texture_) {
+		for (int i = 0; i < SB_HEIGHT; i++) {
+			for (int j = 0; j < SB_WIDTH; j++) {
+				if (tt[i][j] != 0) {
 					//each "ttRect" consists of a Tetrad
 					SDL_Rect ttRect = { x_ + j * 20, y_ + i * 20, w_, h_ };
 					SDL_RenderCopy(renderer_, texture_, nullptr, &ttRect);
 				}
 			}
 		}
-	}
-	else
-	{
+	} else {
 		SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
 		//SDL_RenderFillRect(renderer_, &ttRect);
 	}
 }
 
-void Board::draw(Game* g)
-{
-	if (texture_)
-	{
-		for (int i = 0; i < F_HEIGHT; i++)
-		{
-			for (int j = 0; j < F_WIDTH; j++)
-			{
-				if (g->field_[i][j] != 0)
-				{
+void Board::draw(Game* g) {
+	if (texture_) {
+		for (int i = 0; i < F_HEIGHT; i++) {
+			for (int j = 0; j < F_WIDTH; j++) {
+				if (g->field_[i][j] != 0) {
 					//each "ttRect" consists of a Tetrad
 					SDL_Rect ttRect = { x_ + j * 20, y_ + i * 20, w_, h_ };
 					SDL_RenderCopy(renderer_, texture_, nullptr, &ttRect);
 				}
 			}
 		}
-	}
-	else
-	{
+	} else {
 		SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
 		//SDL_RenderFillRect(renderer_, &ttRect);
+	}
+}
+
+void Board::display(int values) {
+	TTF_Font* font = nullptr;   //opens font style and sets size
+	SDL_Color color = { 0, 0, 0 };
+	std::string str_values = std::to_string(values);
+	const char* cstr = str_values.c_str();
+
+	font = TTF_OpenFont("C:\\Windows\\Fonts\\Arial.ttf", 20);
+	label_ = TTF_RenderText_Solid(font, cstr, color);
+
+	if (font == nullptr)
+		std::cerr << "Font init fail---" << std::endl;
+
+	if (!label_)
+		std::cerr << "Failed to create surface" << std::endl;
+
+	texture_ = SDL_CreateTextureFromSurface(renderer_, label_);
+
+	if (!texture_)
+		std::cerr << "Failed to create texture" << std::endl;
+
+	SDL_QueryTexture(texture_, NULL, NULL, &w_, &h_);
+
+	SDL_Rect board_rect = { x_, y_, w_, h_ };
+
+	if (texture_)
+		SDL_RenderCopy(renderer_, texture_, nullptr, &board_rect);
+	else {
+		SDL_SetRenderDrawColor(renderer_, r_, g_, b_, a_);
+		SDL_RenderFillRect(renderer_, &board_rect);
 	}
 }
