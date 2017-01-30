11a8b0846c8d61834629fa544dcdc50a5bc48da3
parksiwan <parksiwan@users.noreply.github.com>
2017-01-30 21:29:02 +1100

    Update Window.h

--- Window.h
+++ Window.h
@@ -4,11 +4,11 @@
 #include <string>
 #include <SDL.h>
 
+enum INPUT_MESSAGE { NOTHING = 0, START = 1, EXIT = 2, RESTART = 3 };
 const int W_WIDTH = 800;
 const int W_HEIGHT = 600;
 
-class Window 
-{
+class Window {
 public:
 	Window(const std::string& title, int width, int height);
 	~Window();
@@ -16,12 +16,14 @@ public:
 	void pollEvents(SDL_Event& event);
 	void clear() const;
 	inline bool isClosed() const { return closed_; }
+	int getInputMessage();
 
 private:
 	std::string title_;
 	int width_ = W_WIDTH;
 	int height_ = W_HEIGHT;
 	bool closed_ = false;
+	int inputMessage_ = 0;
 	SDL_Window* window_ = nullptr;
 
 protected:
