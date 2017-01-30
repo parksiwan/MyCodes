239deb2fe037b9abb6fcf47d01c7761ace9d9cf4
parksiwan <parksiwan@users.noreply.github.com>
2017-01-30 21:27:21 +1100

    Update Tetrads.cpp

--- Tetrads.cpp
+++ Tetrads.cpp
@@ -2,31 +2,29 @@
 #include <SDL_image.h>
 #include "Tetrads.h"
 
-Tetrads::Tetrads(int type, int rotation) 
-{
+Tetrads::Tetrads(int type, int rotation) {
 	curX_ = 3;                                       //x coordination of far-left of "tetrad_"
 	curY_ = (-1) * startingRow[type][rotation][0];   //y coordination of first row from bottom that is not value of '0'
 	type_ = type;
 	rotation_ = rotation;
 	
 	for (int i = 0; i < HORIZONTAL; i++)
-	{
 		for (int j = 0; j < VERTICAL; j++)
 			tetrad_[i][j] = tetrads[type_][rotation_][i][j];
-	}
+	
+	downEdge_ = edgeLine[type_][rotation_][0];
+	leftEdge_ = edgeLine[type_][rotation_][1];
+	rightEdge_ = edgeLine[type_][rotation_][2];
+	upEdge_ = edgeLine[type_][rotation_][3];
 }
 
-Tetrads::~Tetrads() 
-{
+Tetrads::~Tetrads() {
 
 }
 
-void Tetrads::pollEvents(SDL_Event& event) 
-{
-	if (event.type == SDL_KEYDOWN) 
-	{
-		switch (event.key.keysym.sym) 
-		{
+void Tetrads::pollEvents(SDL_Event& event) {
+	if (event.type == SDL_KEYDOWN) {
+		switch (event.key.keysym.sym) {
 			case SDLK_LEFT:
 				curX_ -= 1;
 				direction_ = LEFT;
@@ -48,12 +46,9 @@ void Tetrads::pollEvents(SDL_Event& event)
 					rotation_ -= 1;
 				break;
 			case SDLK_SPACE:
-				//will be replaced with new logic
-				if (curY_ < 15)
-				{
-					curY_ = 15;
-					direction_ = DOWN;
-				}
+				//drop down
+				curY_ += 1;
+				direction_ = DDOWN;
 				break;
 			case SDLK_ESCAPE:
 				ended_ = true;
@@ -62,35 +57,33 @@ void Tetrads::pollEvents(SDL_Event& event)
 	} 
 }
 
-void Tetrads::movedownTedrads()
-{
+void Tetrads::movedownTedrads() {
 	curY_ += 1;
 	direction_ = DOWN;
 }
 
-void Tetrads::updateTedrads()
-{
+void Tetrads::updateTedrads() {
 	for (int i = 0; i < HORIZONTAL; i++)
-	{
 		for (int j = 0; j < VERTICAL; j++)
 			tetrad_[i][j] = tetrads[type_][rotation_][i][j];
-	}
+	
+	downEdge_  = edgeLine[type_][rotation_][0];
+	leftEdge_  = edgeLine[type_][rotation_][1];
+	rightEdge_ = edgeLine[type_][rotation_][2];
+	upEdge_    = edgeLine[type_][rotation_][3];
 }
 
-bool Tetrads::isEnded()
-{
+bool Tetrads::isEnded() {
 	if (ended_ == true || curY_ == 20)
 		return true;
 
 	return false;
 }
 
-int Tetrads::getCurrentX()
-{
+int Tetrads::getCurrentX() {
 	return curX_;
 }
 
-int Tetrads::getCurrentY()
-{
+int Tetrads::getCurrentY() {
 	return curY_;
 }
