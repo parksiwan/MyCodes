9ef725110692c049b71188e0ce0babaf150eddb4
parksiwan <parksiwan@users.noreply.github.com>
2017-02-08 19:00:37 +1100

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,18 +1,35 @@
+//
+//Header file : Game.h
+//Purpose     : Declaration of Game class 
+//Features    : This class is core of the game. The game is played on this class and controlled 
+//              by this class. All the game rules and counting scores are performed by this class'
+//              member functions.
+//
 #ifndef GAME_H
 #define GAME_H
 
 #include "Tetrads.h"
 
+//F_HEIGHT : height of game field
+//F_WIDTH  : width of game field
+//T_HEIGHT : height of tetrad array
+//T_WIDTH  : width of tetrad array
 const short F_HEIGHT = 20;
 const short F_WIDTH = 10;
 const short T_HEIGHT = 5;
 const short T_WIDTH = 5;
 
+//lines_                    : accumulated number of rows(lines) completed
+//scores_                   : score of game, calculated by 'lines_'
+//level_                    : level of game, default is 1
+//scoringRows_              : number of rows(lines) completed and to be counted as scores
+//field_[F_HEIGHT][F_WIDTH] : game field 10 X 20 array
 class Game {
+	//Board class can accesse and change variables in Game class
 	friend class Board;
 public:
 	Game(int level);
-	~Game();
+	
 	void checkMovement(Tetrads* tts);
 	void updateGame(Tetrads* tts);
 	bool isEnded();
