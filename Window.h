70ffcb5d4e130c02ccb9ab2cd68d806ea152703d
parksiwan <parksiwan@users.noreply.github.com>
2017-02-08 19:03:31 +1100

    Update Window.h

--- Window.h
+++ Window.h
@@ -1,13 +1,27 @@
+//
+//Header file : Window.h
+//Purpose     : Declaration of Window class 
+//Features    : Creates & closes main window, gets user key input and updates window screen
+//
 #ifndef WINDOW_H
 #define WINDOW_H
 
 #include <string>
 #include <SDL.h>
 
+//type of user's input message
 enum INPUT_MESSAGE { NOTHING = 0, START = 1, EXIT = 2, RESTART = 3 };
+//size of window (w x h : 800 x 600)
 const int W_WIDTH = 800;
 const int W_HEIGHT = 600;
 
+//title_        : title of window 
+//width_        : width of window
+//height_       : height of window
+//closed_       : flag indicating window's close
+//inputMessage_ : input message sent by user 
+//window_       : SDL_Window struct pointer holding Window information 
+//renderer_     : SDL_Render struct pointer handling all rendering
 class Window {
 public:
 	Window(const std::string& title, int width, int height);
